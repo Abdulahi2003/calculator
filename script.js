a936d707883d9a88c8b4f79c6680d4d11af7bb3d
Abdulahi <abdulahiaden444@gmail.com>
2024-08-04 21:34:48 -0400

    store all html components in our js, add event listener for number click to show on screen

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
-function add(a,b) {
-    return a + b;
-}
+let previousValue = ''; 
+let currentValue = '';
+let operator = '';
 
-function subtract(a,b) {
-    return a - b;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    let clear = document.querySelector("#clear-btn");
+    let equal = document.querySelector(".equal");
+    let decimals = document.querySelector(".decimal");
 
-function multiply(a,b) {
-    return a * b;
-}
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
 
-function divide(a,b) {
-    return a / b;
-}
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
 
-let num1, num2, operator = "+-*/";
+    numbers.forEach((number) => number.addEventListener("click",function(e) {
+        handleNumber(e.target.textContent);
+        currentScreen.textContent = currentValue;
+    }))
+})
+
+function handleNumber(num) {
+    if (currentValue.length <=5) {
+        currentValue+=num;
+    }
+}
+// function add(a,b) {
+//     return a + b;
+// }
+
+// function subtract(a,b) {
+//     return a - b;
+// }
+
+// function multiply(a,b) {
+//     return a * b;
+// }
+
+// function divide(a,b) {
+//     return a / b;
+// }
 
 function operate() {
-    
+
 }
